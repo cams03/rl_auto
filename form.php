1d32f1fec0e1f1b46eda310e77891371e1d36313
Cams03 <camille.francia@hotmail.fr>
2017-05-22 11:03:33 +0200

    form.php màj

--- form.php
+++ form.php
@@ -7,9 +7,15 @@ $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
 $headers .= 'Content-type: text/html; charset= UTF-8'."\n"; // l'en-tete Content-type pour le format HTML
 $headers .= 'From: <'.$expediteur.'>'."\n"; // Expediteur
 $headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+$demande = $_POST['demande'];
+$marque = $_POST['selectmarque'];
+$modele = $_POST['modele'];
+$annee = $_POST['annee'];
+$km = $_POST['km'];
+$etat = $_POST['etat'];
 $message = '<div style="width: 100%;">'.$_POST['message'].'</div>';
 $retour = '<br><a href="http://camillefrancia.com/index.html" style="text-decoration:none;"><button style="padding:5px; font-family:sans-serif; font-size:15px; margin-top:10px; cursor:pointer;">Retour</button></a>';
-if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
+if (mail($destinataire, $objet, $demande, $marque, $modele, $annee, $km, $etat, $message, $headers)) // Envoi du message
 {
     header('Location: index.html#contact');
 }
